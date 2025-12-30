f639952d776c28d03785667913bfd7a2ba0dc0b3
Mohammed_Alhawry <150382181+Mohammed-Alhawry@users.noreply.github.com>
2025-12-30 14:11:58 +0300

    Remove console log from webhook handler
    Removed console log for webhook hits.

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,7 @@
 // /api/webhook.js
 
 export default async function handler(req, res) {
-  console.log("🔥 WEBHOOK HIT", req.headers, req.body);
+  
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
